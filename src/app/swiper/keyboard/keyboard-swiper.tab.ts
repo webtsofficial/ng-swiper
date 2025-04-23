6dcf3f774c52751dee2bcb66cfcd8c5dddc8cc21
Tobias Schramek <webtsofficial@gmail.com>
2025-04-24 00:36:41 +0200

    added history swiper example

--- src/app/swiper/keyboard/keyboard-swiper.tab.ts
+++ src/app/swiper/keyboard/keyboard-swiper.tab.ts
@@ -3,13 +3,29 @@ import {
     Component,
     ViewEncapsulation,
 } from '@angular/core';
+import {JsonPipe} from '@angular/common';
+import {SwiperComponent, SwiperItemDirective} from 'ng-swiper';
+import {SwiperOptions} from 'swiper/types';
+import {Keyboard} from 'swiper/modules';
 
 @Component({
     selector: 'app-keyboard-swiper',
-    imports: [],
+    imports: [
+        JsonPipe,
+        SwiperComponent,
+        SwiperItemDirective
+    ],
     templateUrl: './keyboard-swiper.tab.html',
     styleUrl: './keyboard-swiper.tab.scss',
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class KeyboardSwiperTab {}
+export class KeyboardSwiperTab {
+    options: SwiperOptions = {
+        modules: [Keyboard],
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true
+        }
+    };
+}
